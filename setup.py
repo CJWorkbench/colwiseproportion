482ae4c3698641ca31418e9719dda7d5a0a976bc
Adam Hooper <adam@adamhooper.com>
2019-08-07 14:00:44 -0400

    Test with Pandas 0.25.0

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup
 
 setup(
-    name='colwiseproportion',
-    version='0.0.1',
-    description='Convert numeric columns into proportions of their totals',
-    author='Adam Hooper',
-    author_email='adam@adamhooper.com',
-    url='https://github.com/CJWorkbench/colwiseproportion',
-    packages=[''],
-    py_modules=['colwiseproportion'],
-    install_requires=['pandas==0.24.2']
+    name="colwiseproportion",
+    version="0.0.1",
+    description="Convert numeric columns into proportions of their totals",
+    author="Adam Hooper",
+    author_email="adam@adamhooper.com",
+    url="https://github.com/CJWorkbench/colwiseproportion",
+    packages=[""],
+    py_modules=["colwiseproportion"],
+    install_requires=["pandas==0.25.0"],
 )
